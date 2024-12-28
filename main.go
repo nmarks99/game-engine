05b4af87afe020c38eb1dbdd6ab31fb86881efd1
Nick Marks <nmarks99@gmail.com>
2024-12-28 11:33:23 -0600

    reset particle position with click

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ func main() {
 	rl.InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Physics Simulation")
 	border := rl.NewRectangle(BORDER_X, BORDER_Y, BORDER_WIDTH, BORDER_HEIGHT)
 
-	const particleX0 float32 = float32(SCREEN_WIDTH) / 2.0
+	const particleX0 float32 = float32(SCREEN_WIDTH) / 6.0
 	const particleY0 float32 = float32(SCREEN_HEIGHT) / 2.0
 	particle := NewParticle(rl.NewVector2(particleX0, particleY0), 20, 1, rl.Blue)
 
@@ -37,7 +37,8 @@ func main() {
 	rl.SetTargetFPS(TARGET_FPS)
     
     t0 := time.Now()
-    var accel float32 = 9.81 * 100.0 // scale to 100 pixels/meter
+    var g float32 = 9.81 * 100.0 // scale to 100 pixels/meter
+    accel := rl.NewVector2(0.0, g)
 
     floorY := border.Height + border.Y
     
@@ -47,25 +48,24 @@ func main() {
         timestamp := time.Since(t0)
 
         // update position and velocity
-        newVel := particle.Velocity.Y + accel * dt
-        newPos := particle.Position.Y + newVel * dt
-        if newPos >= (floorY - particle.Radius) {
-            newPos = floorY - particle.Radius
-            newVel = 0.0
+        newVel := rl.Vector2Add(particle.Velocity, rl.Vector2Scale(accel, dt))
+        newPos := rl.Vector2Add(particle.Position, rl.Vector2Scale(newVel, dt))
+        if newPos.Y >= (floorY - particle.Radius) {
+            newPos.Y = floorY - particle.Radius
+            newVel.Y = 0.0
+            newVel.X = 0.0
         }
-        particle.Velocity.Y = newVel
-        particle.Position.Y = newPos
+        particle.Velocity = newVel
+        particle.Position = newPos
 
-        // Reset ball position and velocity
-        if rl.IsKeyDown(rl.KeySpace) {
-            particle.Position.X = particleX0
-            particle.Position.Y = particleY0
+        if rl.IsMouseButtonReleased(rl.MouseButtonLeft) {
+            particle.Position = mousePos
             particle.Velocity.X = 0.0
             particle.Velocity.Y = 0.0
         }
 
         // text for displaying various info
-        // mousePosText := fmt.Sprintf("Mouse: %.2f, %.2f", mousePos.X, mousePos.Y)
+        mousePosText := fmt.Sprintf("Mouse: %.2f, %.2f", mousePos.X, mousePos.Y)
         particlePosText := fmt.Sprintf("Position: %.2f, %.2f", particle.Position.X, particle.Position.Y)
         particleVelText := fmt.Sprintf("Velocity: %.2f, %.2f", particle.Velocity.X, particle.Velocity.Y)
         fmt.Printf("Time: %.2f\n", float32(timestamp.Milliseconds())/1000.0)
@@ -74,8 +74,9 @@ func main() {
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.RayWhite)
         rl.DrawRectangleLinesEx(border, 5.0, rl.Red)
-        rl.DrawText(particlePosText, 30, 30, 20, rl.Brown)
+        rl.DrawText(particlePosText, 30, 30, 20, rl.Black)
         rl.DrawText(particleVelText, 30, 50, 20, rl.Black)
+        rl.DrawText(mousePosText, 30, 70, 20, rl.Black)
 		quitButton.Draw(mousePos)
 		particle.Draw()
 		rl.EndDrawing()
