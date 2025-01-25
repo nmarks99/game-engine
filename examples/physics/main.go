7c3d4884a79d878d8a2f700ed91c34ab3e88dec9
Nick Marks <nmarks99@gmail.com>
2025-01-25 14:03:14 -0600

    fix formatting

--- examples/physics/main.go
+++ examples/physics/main.go
@@ -15,26 +15,26 @@ func main() {
 
 	// Create a new game instance
 	game := NewGame(SCREEN_WIDTH, SCREEN_HEIGHT, TARGET_FPS)
-    game.SetDamping(0.9)
-    game.SetGravity(NewVector2(0.0, 1000.0))
+	game.SetDamping(0.9)
+	game.SetGravity(NewVector2(0.0, 1000.0))
 
 	// Create a permiter wall (invisible)
-	game.AddPerimiterWall(1, rl.NewColor(0,0,0,0))
+	game.AddPerimiterWall(1, rl.NewColor(0, 0, 0, 0))
 
 	// Cursor texture, hide default cursor
 	cursorTexture := rl.LoadTexture("./assets/cursors/Tiles/tile_0026.png")
 	rl.HideCursor()
 
-    // Texture for the circles
-    ballTexture := rl.LoadTexture("./assets/planets/Terran.png")
+	// Texture for the circles
+	ballTexture := rl.LoadTexture("./assets/planets/Terran.png")
 
 	var mousePos Vector2
 	var mouseVel Vector2
 	var numEntities int
-    const ballRadius float64 = 20.0
-    const boxWidth float64 = 50.0
+	const ballRadius float64 = 20.0
+	const boxWidth float64 = 50.0
 
-    // Custom game update function 
+	// Custom game update function
 	game.SetUpdateCallback(func(game *Game) {
 		mousePos = Vector2FromRaylib(rl.GetMousePosition())
 		mouseVel = Vector2FromRaylib(rl.Vector2Scale(rl.GetMouseDelta(), 50.0))
@@ -43,9 +43,9 @@ func main() {
 		// lift click adds a ball with the velocity of the mouse
 		if rl.IsMouseButtonReleased(rl.MouseButtonLeft) {
 			new_ball := NewPhysicalCircle(mousePos.X, mousePos.Y, ballRadius, 1.0, rl.DarkGreen)
-            new_ball.SetElasticity(0.7)
-            new_ball.SetFriction(0.7)
-            new_ball.SetTexture(ballTexture)
+			new_ball.SetElasticity(0.7)
+			new_ball.SetFriction(0.7)
+			new_ball.SetTexture(ballTexture)
 			new_ball.SetVelocity(mouseVel.X, mouseVel.Y)
 			game.AddEntity(&new_ball)
 		}
@@ -53,8 +53,8 @@ func main() {
 		// right click adds a square with the velocity of the mouse
 		if rl.IsMouseButtonReleased(rl.MouseButtonRight) {
 			new_box := NewPhysicalBox(mousePos.X, mousePos.Y, boxWidth, boxWidth, 1.0, rl.NewColor(39, 81, 130, 255))
-            new_box.SetElasticity(0.7)
-            new_box.SetFriction(0.7)
+			new_box.SetElasticity(0.7)
+			new_box.SetFriction(0.7)
 			new_box.SetVelocity(mouseVel.X, mouseVel.Y)
 			game.AddEntity(&new_box)
 		}
@@ -66,7 +66,7 @@ func main() {
 		}
 	})
 
-    // Custom game draw function 
+	// Custom game draw function
 	game.SetDrawCallback(func(game *Game) {
 		rl.DrawTextureEx(cursorTexture, mousePos.ToRaylib(), 0.0, 2.0, rl.White)
 		rl.DrawText(fmt.Sprintf("Entities: %d\n", numEntities-4), 10, 10, 20, rl.Black)
